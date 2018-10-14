8061324f8f175b4172f22e26d7ee65678204a35b
mGolestan <m.golestan98@gmail.com>
2018-10-15 00:59:36 +0330

    feat - updated winston config

--- src/index.js
+++ src/index.js
@@ -5,7 +5,21 @@ import winston from "winston";
 import loadEnv from "./utils/loadEnv";
 import app from "./app";
 
-winston.level = loadEnv("LOG_LEVEL");
+winston.configure({
+  transports: [
+    new winston.transports.Console({
+      json: false,
+      colorize: true,
+      level: loadEnv("CONSOLE_LOG_LEVEL")
+    }),
+    new winston.transports.File({
+      filename: "./winston.log",
+      json: true,
+      timestamp: true,
+      level: loadEnv("FILE_LOG_LEVEL")
+    })
+  ]
+});
 
 app.listen(loadEnv("PORT"), () => {
   winston.info(`Application started on http://localhost:${loadEnv("PORT")}`);
