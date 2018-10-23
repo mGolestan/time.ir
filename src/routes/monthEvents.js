3712d4864a1011f7fb24acd3e932d4ee256857fa
mGolestan <m.golestan98@gmail.com>
2018-10-23 23:57:54 +0330

    WIP - created new empty route

---
+++ src/routes/monthEvents.js
@@ -0,0 +1,34 @@
+// @flow
+import { promisify } from "util";
+import { Router as router } from "express";
+import { wrap } from "async-middleware";
+import request from "request";
+import { validateParameters } from "../middlewares";
+import errorHandler from "../utils/errorHandler";
+import loadEnv from "../utils/loadEnv";
+
+const route = router();
+const promisifyRequest = promisify(request);
+
+type GetEventRequestType = express$Request & {
+  query: {
+    month: string,
+    year: string
+  }
+};
+
+route.get(
+  "/",
+  validateParameters("query", ["year", "month"]),
+  wrap((req: GetEventRequestType, res: express$Response) => {
+    const { month, year } = req.query;
+
+    const url = `${loadEnv("TIME_IR_MAIN_URL")}/${+year}/${+month}`;
+
+    return promisifyRequest(url)
+      .then((response: { statusCode: number, body: string }) => {}) //eslint-disable-line
+      .catch(() => errorHandler(res));
+  })
+);
+
+export default route;
